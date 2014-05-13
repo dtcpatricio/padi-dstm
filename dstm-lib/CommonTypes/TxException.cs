c97c5f40c2b501389f53f502b313abb783ae5127
dtcpatricio <duarte.carvalho@ist.utl.pt>
2014-05-13 21:21:47 +0100

    Started a bit of work on the TxException.

--- dstm-lib/CommonTypes/TxException.cs
+++ dstm-lib/CommonTypes/TxException.cs
@@ -6,16 +6,29 @@ using System.Threading.Tasks;
 
 namespace CommonTypes
 {
+    [Serializable]
     public class TxException : ApplicationException
     {
         private string _msg;
 
-        public string getMessage { get { return _msg; } }
+        public string GETMESSAGE { get { return _msg; } }
 
         public TxException(string msg)
         {
             _msg = msg;
         }
 
+        public TxException(System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context)
+            : base(info, context)
+        {
+            _msg = info.GetString("_msg");
+        }
+
+        public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("_msg", _msg);
+        }
     }
 }
